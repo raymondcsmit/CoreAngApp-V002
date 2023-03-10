68a49efce3462df8341542b086de9846ef595ae6
Waqar Habib <waqarhabibmit@gmail.com>
2023-03-10 08:53:18 +0500

    update working

--- CoreAngApp/ClientApp/src/app/builder/list-ag-grid.component.ts
+++ CoreAngApp/ClientApp/src/app/builder/list-ag-grid.component.ts
@@ -5,138 +5,66 @@ import {
   FormGroup,
   Validators,
 } from "@angular/forms";
-import { ColDef , GridOptions } from "ag-grid-community";
-import {  delay, Observable, of } from "rxjs";
+import { ColDef, GridOptions } from "ag-grid-community";
+import { delay, Observable, of } from "rxjs";
 import { BaseComponent } from "./base.component";
 
 @Component({
   selector: "app-ag-grid",
   template: `
-  <h3>{{formName}}</h3>
-  <ag-grid-angular
-  style="width: 100%; height: 200px;"
-  class="ag-theme-alpine"
-  [columnDefs]="columnDefs"
-  [rowData]="listData$ | async"
-></ag-grid-angular>
+    <h3>{{ formName }}</h3>
+    <div class="parent-element">
+    <ag-grid-angular
+      style="width: 100%; height: 200px;"
+      class="ag-theme-alpine"
+      [columnDefs]="columnDefs"
+      [rowData]="listData$ | async"
+    ></ag-grid-angular>
+</div>
   `,
+styles: [`
+.parent-element {
+  height: calc(100vh - 150px);
+  padding: 10px;
+}
+`]
 })
 export class ListAgGridComponent extends BaseComponent implements OnInit {
-
-  constructor(
-    fb: FormBuilder
-  ) {
+  constructor(fb: FormBuilder) {
     super(fb);
   }
   @Input() key: string = this.configuration?.name;
-  rowData$: Observable<any[]> = this.loadData();
-   rowData:any[]= [
-    {field1: 'ewe1', field2: '1ewew', field3: '2'},
-    {field1: 'ewe2', field2: '2ewew', field3: '2'},
-    {field1: 'ewe3', field2: '3ewew', field3: '2'}
-  ];
-columnDefs: any[] = [
-  { headerName: 'Field 1', field: 'field1', minWidth: 100 },
-  { headerName: 'Field 2', field: 'field2', minWidth: 100 },
-  { headerName: 'Field 3', field: 'field3', minWidth: 100 },
-];
-  
-  public gridOptions: GridOptions = {
-  pagination: true,
-  rowHeight: 50,
-  enableRangeSelection: true,
-  statusBar:  {
-    statusPanels: [
-      { statusPanel: 'statusBarComponent', align: 'center' },
-      { statusPanel: 'agTotalRowCountComponent', align: 'left', key: 'statusBarCompKeyRowCount' }, // Total row count,
-      { statusPanel: 'agFilteredRowCountComponent' }, // Count after filtering
-      { statusPanel: 'agSelectedRowCountComponent' }, // Selected row count
-      {
-        statusPanel: 'agAggregationComponent', // Summary information Average, Count, Min, Max and Sum
-        key: 'statusBarCompKey',
-        statusPanelParams: {
-          aggFuncs: ['sum', 'count', 'min', 'max', 'avg']
-        }
-      }
-    ]
-  },
-  
-   defaultColDef: {
-    enableValue: true,
-    sortable:true,
-    enableRowGroup: true,
-    enablePivot: true,
-    // editable: true
-  }
-}
 
-  listData1?: Observable<any[]>;
-  vrtdata?:any[];
-  
-  loadData(): Observable<any[]> {
-    const students: any[] = [
-      {field1: 'ewe1', field2: '1ewew', field3: '2'},
-      {field1: 'ewe2', field2: '2ewew', field3: '2'},
-      {field1: 'ewe3', field2: '3ewew', field3: '2'}
-    ];
-    return of(students).pipe(delay(500));
-  }
+  columnDefs: any[] = [];
 
   saveAction() {
     this.actionPerformed.emit();
   }
   ngOnInit() {
-    if (this.configuration && this.configuration.displayedColumns && this.configuration.fields) {
-      this.columnDefs = this.configuration.displayedColumns.map((column) => {
-        const field = this.configuration.fields?.find((fd) => fd.name === column);
-        if (field) {
-          return {
-            headerName: field.label,
-            field: field.name,
-          };
-        } else {
-          return null;
-        }
-      }).filter((def) => def !== null);
+    if (
+      this.configuration &&
+      this.configuration.displayedColumns &&
+      this.configuration.fields
+    ) {
+      this.columnDefs = this.configuration.displayedColumns
+        .map((column) => {
+          const field = this.configuration.fields?.find(
+            (fd) => fd.name === column
+          );
+          if (field) {
+            return {
+              headerName: field.label,
+              field: field.name,
+            };
+          } else {
+            return null;
+          }
+        })
+        .filter((def) => def !== null);
     } else {
       this.columnDefs = [];
     }
-    
-    // this.columnDefs = this.configuration?.displayedColumns?.map((column) => {
-    //   const field = this.configuration?.fields?.find((fd) => fd.name === column);
-    //   let columnDef = {
-    //     headerName: field.label,
-    //     field: field.name,
-    //   };
-    //   // if (field.type === 'select') {
-    //   //   columnDef = {
-    //   //     ...columnDef,
-    //   //     cellRenderer: 'agSelectCellRenderer',
-    //   //     cellEditor: 'agSelectCellEditor',
-    //   //     cellEditorParams: {
-    //   //       values: field.options.reduce((acc, option) => {
-    //   //         acc[option.value] = option.label;
-    //   //         return acc;
-    //   //       }, {}),
-    //   //     },
-    //   //   };
-    //   // }
-
-    //   return columnDef;
-    // });
-
-
-
-    this.listData1=this.loadData();
-    this.listData1.subscribe((data:any[])=>{
-    console.log('Column Definition',this.columnDefs);
-    this.vrtdata = data;
-        this.gridOptions.rowData=data; 
-        this.gridOptions.columnDefs =this.columnDefs
-      console.log('row data is : ',data);
-    })
   }
-  
 
   getValidators(field: any) {
     const validators = [];
