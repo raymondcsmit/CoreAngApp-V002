15856e98d626457e833d46de13d525e3d04e06cb
Waqar Habib <waqarhabibmit@gmail.com>
2023-03-11 07:29:58 +0500

    Export PDF Enabled on Ag-Grid

--- CoreAngApp/ClientApp/src/app/builder/list-ag-grid.component.ts
+++ CoreAngApp/ClientApp/src/app/builder/list-ag-grid.component.ts
@@ -5,9 +5,12 @@ import {
   FormGroup,
   Validators,
 } from "@angular/forms";
-import { ColDef, CsvExportParams, ExcelExportParams, GridApi, GridOptions, GridReadyEvent } from "ag-grid-community";
+import { ColDef, ColumnApi, CsvExportParams, ExcelExportParams, GridApi, GridOptions, GridReadyEvent } from "ag-grid-community";
 import { delay, Observable, of } from "rxjs";
 import { BaseComponent } from "./base.component";
+import printDoc from "./pdfBuilder/printDoc.js";
+
+//import printDoc from "pdfBuilder/printDoc";
 
 @Component({
   selector: "app-ag-grid",
@@ -20,6 +23,12 @@ import { BaseComponent } from "./base.component";
       >
         Export to Excel
       </button>
+      <button (click)="exportAsPdf($event)" 
+          matTooltip="Export to PDF"
+          mat-raised-button>
+          Export as PDF
+            <mat-icon>get_app</mat-icon>
+        </button>
     </div>
     
     <ag-grid-angular
@@ -48,7 +57,44 @@ export class ListAgGridComponent extends BaseComponent implements OnInit {
   gridOptions: any;
   columnDefs: any[] = [];
   private gridApi!: GridApi<any>;
-  
+  private gridColumnApi!: ColumnApi;
+  //#region Pdf settings
+  PDF_HEADER_COLOR = "#f8f8f8";
+  PDF_INNER_BORDER_COLOR = "#dde2eb";
+  PDF_OUTER_BORDER_COLOR = "#babfc7";
+  PDF_LOGO =
+    "https://raw.githubusercontent.com/AhmedAGadir/ag-grid-todo-list-react-typescript/master/src/assets/new-ag-grid-logo.png";
+  PDF_PAGE_ORITENTATION = "landscape";
+  PDF_WITH_HEADER_IMAGE = true;
+  PDF_WITH_FOOTER_PAGE_COUNT = true;
+  PDF_HEADER_HEIGHT = 25;
+  PDF_ROW_HEIGHT = 15;
+  PDF_ODD_BKG_COLOR = "#fcfcfc";
+  PDF_EVEN_BKG_COLOR = "#ffffff";
+  PDF_WITH_CELL_FORMATTING = true;
+  PDF_WITH_COLUMNS_AS_LINKS = true;
+  PDF_SELECTED_ROWS_ONLY = false;
+  exportAsPdf(event:any): void {
+    event.preventDefault();
+      const printParams = {
+        PDF_HEADER_COLOR: this.PDF_HEADER_COLOR,
+        PDF_INNER_BORDER_COLOR: this.PDF_INNER_BORDER_COLOR,
+        PDF_OUTER_BORDER_COLOR: this.PDF_OUTER_BORDER_COLOR,
+        PDF_LOGO: this.PDF_LOGO,
+        PDF_PAGE_ORITENTATION: this.PDF_PAGE_ORITENTATION,
+        PDF_WITH_HEADER_IMAGE: this.PDF_WITH_HEADER_IMAGE,
+        PDF_WITH_FOOTER_PAGE_COUNT: this.PDF_WITH_FOOTER_PAGE_COUNT,
+        PDF_HEADER_HEIGHT: this.PDF_HEADER_HEIGHT,
+        PDF_ROW_HEIGHT: this.PDF_ROW_HEIGHT,
+        PDF_ODD_BKG_COLOR: this.PDF_ODD_BKG_COLOR,
+        PDF_EVEN_BKG_COLOR: this.PDF_EVEN_BKG_COLOR,
+        PDF_WITH_CELL_FORMATTING: this.PDF_WITH_CELL_FORMATTING,
+        PDF_WITH_COLUMNS_AS_LINKS: this.PDF_WITH_COLUMNS_AS_LINKS,
+        PDF_SELECTED_ROWS_ONLY: this.PDF_SELECTED_ROWS_ONLY
+      };
+      printDoc(printParams, this.gridApi, this.gridColumnApi);
+    }
+  //#endregion Pdf settings
   // gridOptions: GridOptions = {
   //   enableExcelExport: true,
   // };
@@ -68,13 +114,10 @@ export class ListAgGridComponent extends BaseComponent implements OnInit {
     fileName: `${this.formName}exportedfile`
     }
     this.gridApi.exportDataAsCsv(params);
-    // const params: ExcelExportParams = {
-    //   fileName: 'exported_data.xlsx',
-    // };
-    // this.gridApi.exportDataAsExcel(params);
   }
   
   onGridReady(params: GridReadyEvent<any>) {
+    this.gridColumnApi = params.columnApi;
     this.gridApi = params.api;
   }
   ngOnInit() {
---
+++ CoreAngApp/ClientApp/src/app/builder/pdfBuilder/printDoc.d.ts
@@ -0,0 +1,1 @@
+export default function printDoc(printParams: any, gridApi: any, columnApi: any): void;
